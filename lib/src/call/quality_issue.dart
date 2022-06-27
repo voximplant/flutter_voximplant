ffc0d86e674d0b2e5c89698e1340cb9f621036eb
Ilya Petrosyan <im.clean@yandex.ru>
2022-06-27 10:45:26 +0300

    Update  description classes

--- lib/src/call/quality_issue.dart
+++ lib/src/call/quality_issue.dart
@@ -88,7 +88,16 @@ class FrameSize {
 abstract class QualityIssueEvent {}
 
 /// Represents class reporting that local video is encoded by a codec
-/// different from specified in [VICallSettings.preferredVideoCodec]
+/// different from specified in `VICallSettings.preferredVideoCodec`.
+///
+/// Issue level is `VIQualityIssueLevel.VIQualityIssueLevelCritical` if video
+/// is not sent, `VIQualityIssueLevel.VIQualityIssueLevelMajor` in case of
+/// codec mismatch or `VIQualityIssueLevel.VIQualityIssueLevelNone` if the issue
+/// is not detected.
+///
+/// Possible reasons:
+/// * The video is not sent for some reasons. In this case codec will be null
+/// * Different codecs are specified in the call endpoints
 class CodecMismatch implements QualityIssueEvent {
   // Codec that is currently used or null if the video is not sent
   final String? codec;
@@ -100,7 +109,16 @@ class CodecMismatch implements QualityIssueEvent {
 }
 
 /// Represents class reporting that video resolution sent to the endpoint
-/// is lower than a captured video resolution.
+/// is lower than a captured video resolution. As a result it affects remote
+/// video quality on the remote participant side, but do not affect the quality
+/// of local video preview on the android application.
+///
+/// The issue level may vary during the call.
+///
+/// Possible reasons:
+///
+/// * High CPU load during the video call
+/// * Network issues such as poor internet connection or low bandwidth
 class LocalVideoDegradation implements QualityIssueEvent {
   /// Sent frame size.
   final FrameSize actualSize;
@@ -120,6 +138,17 @@ class LocalVideoDegradation implements QualityIssueEvent {
 
 /// Represents class reporting that network-based media latency
 /// is detected in the call.
+/// Network-based media latency is calculated based on rtt (round trip time)
+/// and jitter buffer. Latency refers to the time it takes a voice/video packet
+/// to reach its destination. Sufficient latency causes call participants
+/// to speak over the top of each other.
+///
+/// Issue level may vary during the call.
+///
+/// Possible reasons:
+///
+/// * Network congestion/delays
+/// * Lack of bandwidth
 class HighMediaLatency implements QualityIssueEvent {
   /// Network-based latency measured in milliseconds at the moment
   /// the issue triggered.
@@ -133,6 +162,16 @@ class HighMediaLatency implements QualityIssueEvent {
 
 /// Represents class reporting that ICE connection is switched to the
 /// "disconnected" state during the call
+///
+/// Issue level is always `VIQualityIssueLevel.VIQualityIssueLevelCritical`,
+/// because there is no media in the call until the issue is resolved.
+///
+/// Event may be triggered intermittently and be resolved just as spontaneously
+/// on less reliable networks, or during temporary disconnections.
+///
+/// Possible reasons:
+///
+/// * Network issues
 class IceDisconnected implements QualityIssueEvent {
   /// Issue level
   final VIQualityIssueLevel level;
@@ -141,6 +180,15 @@ class IceDisconnected implements QualityIssueEvent {
 }
 
 /// Represents class reporting that no audio is captured by the microphone.
+///
+/// Issue level can be only `VIQualityIssueLevel.VIQualityIssueLevelCritical`
+/// if the issue is detected or VIQualityIssueLevelNone if the issue is not
+/// detected or resolved.
+///
+/// Possible reasons:
+///
+/// * Access to microphone is denied
+/// * Category of AVAudioSession is not AVAudioSessionCategoryPlayAndRecord
 class NoAudioSignal implements QualityIssueEvent {
   /// Issue level
   final VIQualityIssueLevel level;
@@ -150,7 +198,16 @@ class NoAudioSignal implements QualityIssueEvent {
   });
 }
 
-/// Represents class reporting that packet loss detection.
+/// Represents class reporting that packet loss detection. Packet loss can lead
+/// to missing of entire sentences, awkward pauses in the middle of a
+/// conversation or robotic voice during the call.
+///
+/// Issue level may vary during the call.
+///
+/// Possible reasons:
+///
+/// * Network congestion
+/// * Bad hardware (parts of the network infrastructure)
 class PacketLoss implements QualityIssueEvent {
   /// Average packet loss for 2.5 seconds.
   final double packetLoss;
@@ -166,6 +223,30 @@ class PacketLoss implements QualityIssueEvent {
 
 /// Represents class reporting that no audio is received on the
 /// remote audio stream.
+///
+/// Issue level can be only `VIQualityIssueLevel.VIQualityIssueLevelCritical`
+/// if the issue is detected or `VIQualityIssueLevel.VIQualityIssueLevelNone`
+/// if the issue is not detected or resolved.
+///
+/// If no audio receive is detected on several remote audio streams,the
+/// event will be invoked for each of the remote audio streams with the issue.
+///
+/// If the issue level is `VIQualityIssueLevel.VIQualityIssueLevelCritical`
+/// the event will not be invoked with the level VIQualityIssueLevelNone in cases:
+///
+/// * The (conference) call ended
+/// * The endpoint left the conference call -
+/// [VIEndpoint.onEndpointRemoved] is invoked
+///
+/// The issue is not detected for the following cases:
+///
+/// * The endpoint put the call on hold via [VICall.hold]
+/// * The endpoint stopped sending audio during a call via [VICall.sendAudio]
+///
+/// Possible reasons:
+///
+/// * Poor internet connection on the client or the endpoint
+/// * Connection lost on the endpoint
 class NoAudioReceive implements QualityIssueEvent {
   /// Id remote audio stream the issue occured on.
   final String audiostreamId;
@@ -185,6 +266,36 @@ class NoAudioReceive implements QualityIssueEvent {
 
 /// Represents class reporting that no video is received on the
 /// remote video stream.
+///
+/// Issue level can be only `VIQualityIssueLevel.VIQualityIssueLevelCritical`
+/// if the issue is detected or `VIQualityIssueLevel.VIQualityIssueLevelNone`
+/// if the issue is not detected or resolved.
+///
+/// If no video receive is detected on several remote video streams,the
+/// event will be invoked for each of the remote video streams with the issue.
+///
+/// If the issue level is `VIQualityIssueLevel.VIQualityIssueLevelCritical`
+/// the event will not be invoked with the level VIQualityIssueLevelNone in cases:
+///
+/// * The (conference) call ended
+/// * The remote video stream was removed -
+/// [VIEndpoint.onRemoteVideoStreamRemoved] is invoked
+/// * The endpoint left the conference call -
+/// [VIEndpoint.onEndpointRemoved] is invoked
+///
+/// The issue is not detected for the following cases:
+///
+/// * The endpoint put the call on hold via [VICall.hold]
+/// * The endpoint stopped sending audio during a call via [VICall.sendVideo]
+/// * Video receiving was stopped on the remote video stream via
+/// [VIEndpoint.stopReceiving]
+///
+/// Possible reasons:
+///
+/// * Poor internet connection on the client or the endpoint
+/// * Connection lost on the endpoint
+/// * The endpoint's application has been moved to the background state on an
+/// iOS device (camera usage is prohibited while in the background on iOS)
 class NoVideoReceive implements QualityIssueEvent {
   /// Id remote video stream the issue occured on.
   final String videostreamId;
