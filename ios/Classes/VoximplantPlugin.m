535c291367600b290b59e88426b398573f5dbdb7
YuliaGrigorieva <miss000hara@gmail.com>
2023-08-29 12:46:49 +0300

    Update changelog and sdk version to 3.8.3

--- ios/Classes/VoximplantPlugin.m
+++ ios/Classes/VoximplantPlugin.m
@@ -59,7 +59,7 @@
         self.audioFileManager = [[VIAudioFileManager alloc] initWithPlugin:self];
         self.cameraModule = [[VICameraModule alloc] init];
         self.messagingModule = [[VIMessagingModule alloc] initWithRegistrar:self.registrar];
-        [VIClient setVersionExtension:@"flutter-3.8.2"];
+        [VIClient setVersionExtension:@"flutter-3.8.3"];
     }
     return self;
 }
