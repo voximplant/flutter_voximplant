b064acb44b0c6e9b13573c3e0fe17d7fc5ae4bae
YuliaGrigorieva <miss000hara@gmail.com>
2024-05-28 16:52:43 +0300

    remove deprecated test_drive tests

--- example/test/test_user.dart
+++
@@ -1,9 +0,0 @@
-/// Copyright (c) 2011-2020, Zingaya, Inc. All rights reserved.
-
-class TestUser {
-  final String name;
-  final int id;
-  final String pass;
-
-  const TestUser(this.name, this.id, this.pass);
-}
